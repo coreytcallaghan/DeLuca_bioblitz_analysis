aadf2021b96eb3df9569bfc578013a98f76d0690
Levi Hoskins <levi21342@outlook.com>
2026-01-16 08:01:47 -0500

    fixing accum_plot_RG

--- Code/analysis_of_data.R
+++ Code/analysis_of_data.R
@@ -77,7 +77,7 @@ first_obs_species_all <- deluca_bioblitz %>%
   mutate(cumulative_species = cumsum(new_species))
 
 accum_all_plot <- ggplot(first_obs_species_all, aes(x = observed_on)) +
-  geom_col(aes(y = new_species), fill = "darkgoldenrod1", alpha = 0.6, width = 4) +
+  geom_col(aes(y = new_species), fill = "black", alpha = 0.6, width = 4) +
   geom_line(aes(y = cumulative_species), color = "darkgoldenrod1", size = 1.2) +
   geom_point(aes(y = cumulative_species), size = 2, color = "darkgoldenrod1") +
   xlab("Sampling Date") +
@@ -90,24 +90,34 @@ accum_all_plot <- ggplot(first_obs_species_all, aes(x = observed_on)) +
   )
 
 # plot an accumulation type plot of new species
-# now for RG only!
+# filter to RG
 first_obs_species_RG <- deluca_bioblitz %>%
-  dplyr::select(taxon_genus_name, taxon_species_name, observed_on, quality_grade) %>%
-  dplyr::filter(quality_grade == "research") %>%
-  dplyr::filter(complete.cases(taxon_species_name)) %>%
-  group_by(taxon_species_name, observed_on) %>%
-  arrange(desc(observed_on)) %>%
-  distinct() %>%
-  group_by(observed_on) %>%
-  summarise(new_species = n_distinct(taxon_species_name), .groups = "drop") %>%
+  filter(
+    quality_grade == "research",
+    !is.na(taxon_species_name)
+  ) %>%
+  group_by(taxon_species_name) %>%
+  summarise(
+    first_date = min(observed_on),
+    .groups = "drop"
+  )
+
+# count number of species 
+accum_data_RG <- first_obs_species_RG %>%
+  group_by(first_date) %>%
+  summarise(new_species = n(), .groups = "drop") %>%
+  arrange(first_date) %>%
   mutate(cumulative_species = cumsum(new_species))
 
-accum_RG_plot <- ggplot(first_obs_species_RG, aes(x = observed_on)) +
-  geom_col(aes(y = new_species), fill = "black", alpha = 1, width = 6) +
+# now plot
+accum_RG_plot <- ggplot(accum_data_RG, aes(x = first_date)) +
+  geom_col(aes(y = new_species), fill = "black", width = 6) +
   geom_line(aes(y = cumulative_species), color = "darkgoldenrod1", size = 1.2) +
-  geom_point(aes(y = cumulative_species), size = 2, color = "darkgoldenrod1") +
-  xlab("Sampling Date") +
-  ylab("Number of species") +
+  geom_point(aes(y = cumulative_species), color = "darkgoldenrod1", size = 2) +
+  labs(
+    x = "Sampling Date",
+    y = "Number of species"
+  ) +
   theme_bw() +
   theme(
     panel.grid = element_blank(),
